9646fdde7e2575ae84373a318a317c63f26647c5
Natnael Tedros <86560822+natitedros@users.noreply.github.com>
2021-10-24 22:36:22 +0300

    Update selectionSort.java

--- selectionSort.java
+++ selectionSort.java
@@ -14,5 +14,8 @@ public class selectionSort {
                 arr[i] = val;
             }
         }
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
     }
 }
