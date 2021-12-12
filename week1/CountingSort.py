586708254317f96bdaeb1aa976cd3b1d9dfc9b1c
Natnael Tedros <86560822+natitedros@users.noreply.github.com>
2021-12-13 00:07:43 +0300

    Update CountingSort.py

--- week1/CountingSort.py
+++ week1/CountingSort.py
@@ -1,6 +1,6 @@
 def countingSort(arr):
     # Write your code here
-    counter = [0]*len(arr)
+    counter = [0]*(max(arr)+1)
     for i in range(len(arr)):
         counter[arr[i]]+=1
     return counter
