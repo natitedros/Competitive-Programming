f453aa6b541380e174076e515db4ea14d38ee9d5
Nati_Tedros <tedros.natnael@gmail.com>
2021-12-14 18:45:11 +0300

    merged

--- week1/SelectionSort.py
+++ week1/SelectionSort.py
@@ -3,9 +3,11 @@ class Solution:
         # code here 
         minPos = i
         for j in range(i, len(arr)):
-            if arr[j]<arr[i]:
+            if arr[j]<arr[minPos]:
                 minPos = j
         return minPos
     def selectionSort(self, arr,n):
         #code here
-        arr.sort()
+        for i in range(len(arr)):
+            minIndex = self.select(arr, i)
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
