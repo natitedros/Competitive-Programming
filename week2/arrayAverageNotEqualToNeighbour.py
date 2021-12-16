0ddd8ade82444358a59fef4532c61188203b541a
Nati_Tedros <tedros.natnael@gmail.com>
2021-12-16 18:46:28 +0300

    Submitted

---
+++ week2/arrayAverageNotEqualToNeighbour.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        for i in range(0,len(nums)-1,2):
+            nums[i], nums[i+1] = nums[i+1], nums[i]
+            
+        return nums
