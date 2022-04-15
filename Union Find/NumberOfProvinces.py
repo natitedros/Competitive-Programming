6213cdee768b4bd1fb2d50961c5a01c84fdf0c32
Nati_Tedros <tedros.natnael@gmail.com>
2022-04-16 00:24:20 +0300

    Added

---
+++ Union Find/NumberOfProvinces.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        parent = [0]*n
+        province = {}
+        joined = set()
+        for i in range(n):
+            parent[i] = i
+            province[i] = [i]
+        def find(city):
+            return parent[city]
+        def union(one, two):
+            parentOne = find(one)
+            parentTwo = find(two)
+            if parentOne == parentTwo:
+                return 0
+            if len(province[parentTwo]) > len(province[parentOne]):
+                parentTwo, parentOne = parentOne, parentTwo
+            for i in range(len(province[parentTwo])):
+                parent[province[parentTwo][i]] = parentOne
+                province[parentOne].append(province[parentTwo][i])
+            del province[parentTwo]
+        for i in range(n):
+            for j in range(n):
+                if i != j and (i,j) not in joined and isConnected[i][j] == 1:
+                    joined.add((j,i))
+                    print(i,j)
+                    union(i, j)
+        return len(province)
