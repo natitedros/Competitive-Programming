df417a93dfb5dcfc836f47ba9c07684e2027423d
Nati_Tedros <tedros.natnael@gmail.com>
2022-05-06 23:02:05 +0300

    played with

--- PlaygroundCodes/repeatedString.py
+++ PlaygroundCodes/repeatedString.py
@@ -1,7 +1,3 @@
-arr = s*(math.ceil(n/len(s)))
-    arr = ''.join(arr)
-    counter = 0
-    for i in range(0,n,1):
-        if arr[i]=="a":
-            counter += 1
-    return counter
+res = []
+res.append('a'*5)
+print(res)
