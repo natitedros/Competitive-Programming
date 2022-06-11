d39a2883d6b2dabd69f4cb2c1d16ec06ec2f8d58
Nati_Tedros <tedros.natnael@gmail.com>
2022-06-11 10:00:57 +0300

    Added

---
+++ campContest/HelpfulMaths.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+q = input()
+question = q.strip().split('+')
+question.sort()
+print('+'.join(question))
+
