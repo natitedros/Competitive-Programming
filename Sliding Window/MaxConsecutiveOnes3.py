e35c7271d8db10973f39a0b497719a273f54649b
Nati_Tedros <tedros.natnael@gmail.com>
2022-07-21 12:11:01 +0300

    Added

--- Sliding Window/MaxConsecutiveOnes3.py
+++ Sliding Window/MaxConsecutiveOnes3.py
@@ -1,24 +1,13 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        l = len(nums)
-        chance = k
-        maxLength = 0
-        left = 0
-        right = 0
-        while right < l:
-            if left == right:
-                chance = k
-            if not chance and nums[right] == 0:
-                if right-left > maxLength:
-                    maxLength = right-left
-                if nums[left] == 0:
-                    chance += 1
+        left,right,res = 0,0,0
+        while right < len(nums):
+            if not nums[right]:
+                k -= 1
+            while k<0:
+                if not nums[left]:
+                    k += 1
                 left += 1
-            elif nums[right] == 0:
-                chance -= 1
-                right += 1
-            else:
-                right += 1
-        if right-left > maxLength:
-            maxLength = right-left
-        return maxLength 
+            res = max(res,right-left+1)
+            right += 1
+        return res
