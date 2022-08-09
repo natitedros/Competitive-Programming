8ae434507d336f1728443004e25f22ed53fe53bd
Nati_Tedros <tedros.natnael@gmail.com>
2022-08-09 10:58:53 +0300

    Added

--- DP/LongestIncreasingSubsequence.py
+++ DP/LongestIncreasingSubsequence.py
@@ -1,3 +1,5 @@
+# Using DP 
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         l = len(nums)
@@ -15,3 +17,27 @@ class Solution:
         for i in range(l):
             mx = max(mx,incSubsequence(i))
         return mx
+
+
+# Using Binary Search 
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        l = len(nums)
+        sub = []
+        for i in range(l):
+            if not sub or nums[i] > sub[-1]:
+                sub.append(nums[i])
+            elif nums[i] <= sub[0]:
+                sub[0] = nums[i]
+            else:
+                left = 0
+                right = len(sub)-1
+                mid = 0
+                while left < right:
+                    mid = left + (right-left)//2
+                    if sub[mid] < nums[i]:
+                        left = mid+1
+                    else:
+                        right = mid
+                sub[right] = nums[i]
+        return len(sub)
