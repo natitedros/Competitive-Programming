133b2de31a4a67c3ec6cdac7ede020d3367cb367
Nati_Tedros <tedros.natnael@gmail.com>
2022-08-10 20:22:51 +0300

    Added

--- campContest/ConsecutiveSubsequence.py
+++ campContest/ConsecutiveSubsequence.py
@@ -14,7 +14,21 @@ def invr():
     return(map(int,input().split()))
 n = inp()
 nums = inlt()
-freq = defaultdict(int)
-d = {}
+length = defaultdict(int)
+index = {}
 for i in range(n):
-    freq[nums[i]] += 1
+    length[nums[i]] = 1+length[nums[i]-1]
+    if nums[i]-1 in index:
+        index[nums[i]] = index[nums[i]-1]
+        index[nums[i]].append(i)
+    else:
+        index[nums[i]] = [i]
+maxLen = 0
+maxIndex = []
+for i in range(n):
+    if length[nums[i]] > maxLen:
+        maxLen = length[nums[i]]
+        maxIndex = index[nums[i]]
+print(maxLen)
+for i in range(maxLen):
+    print(maxIndex[i]+1, end=" ")
