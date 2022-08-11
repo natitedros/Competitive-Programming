ec8199c1e8e7e74f2525d2f9d20589732636bb9b
Nati_Tedros <tedros.natnael@gmail.com>
2022-08-11 12:01:44 +0300

    Edited

--- Binary Search/FindFirstAndLastPosition.py
+++ Binary Search/FindFirstAndLastPosition.py
@@ -23,3 +23,29 @@ class Solution:
             if (left<0 or nums[left] != target) and (right >=len(nums) or nums[right] != target):
                 found = False
                 return [left+1,right-1]
+#Improved
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(nums)-1
+        found = False
+        while left <= right:
+            mid = left + (right-left)//2
+            if nums[mid] < target:
+                left = mid+1
+            elif nums[mid] > target:
+                right = mid-1
+            else:
+                found = True
+                left = right = mid
+                break
+        if not found:
+            return [-1,-1]
+        res = [-1,-1]
+        while left>=0 and nums[left]==target:
+            left -= 1
+        res[0] = left+1
+        while right<len(nums) and nums[right]==target:
+            right += 1
+        res[1] = right-1
+        return res
