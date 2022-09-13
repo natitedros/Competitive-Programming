0b5f6ca55cbbc2c55f1dae8585c432e04954074c
Nati_Tedros <tedros.natnael@gmail.com>
2022-09-13 11:31:59 +0300

    Added

---
+++ DP/UniquePaths.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = defaultdict(int)
+        dp[(m-1,n-1)] = 1
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                dp[(i,j)] += dp[(i+1,j)] + dp[(i,j+1)]
+        return dp[(0,0)]
