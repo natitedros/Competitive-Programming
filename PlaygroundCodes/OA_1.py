ebf620640234e81a4ee327a6f9be50c2e01ee0b3
Nati_Tedros <tedros.natnael@gmail.com>
2022-11-15 20:23:59 +0300

    Edited

--- PlaygroundCodes/OA_1.py
+++ PlaygroundCodes/OA_1.py
@@ -1,10 +1,11 @@
 def solve(s):
     result = 0
-    for i in range(1, len(s)):
+    prev = 2
+    for i in range(len(s)):
         temp = ord(s[i]) - 97
-        prev = ord(s[i-1]) - 97
         if prev == temp:
             result += 2
         elif prev == (temp+1)%3:
             result += 1
-    return result
+        prev = temp
+    return result + (2-prev)
